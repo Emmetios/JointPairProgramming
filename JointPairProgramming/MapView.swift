50de2b4859a8abe57248131d94cea0f7dc6d2f6a
code-Adrian-VHI <adrian.bernacki@vhi.ie>
2023-10-11 12:27:44 +0100

    Map Change commit

---
+++ JointPairProgramming/MapView.swift
@@ -0,0 +1,28 @@
+//
+//  MapView.swift
+//  JointPairProgramming
+//
+//  Created by Adrian Bernacki on 11/10/2023.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var body: some View {
+        Map(initialPosition: .region(region))
+    }
+    
+}
+
+private var region: MKCoordinateRegion{
+    MKCoordinateRegion(
+    center: CLLocationCoordinate2D(latitude: 34.001_286, longitude: -116.166_868),
+    span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    )
+}
+
+
+#Preview {
+    MapView()
+}
