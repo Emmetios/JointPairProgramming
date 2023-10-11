c578cd69d0d0359a556f35e34e28c07ec272eed9
Emmet Malone <emmetmalone@Emmets-MacBook-Pro.local>
2023-10-11 10:12:32 +0100

    first commit

---
+++ JointPairProgramming/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JointPairProgramming
+//
+//  Created by Emmet Malone on 11/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JointPairProgramming/JointPairProgrammingApp.swift
@@ -0,0 +1,17 @@
+//
+//  JointPairProgrammingApp.swift
+//  JointPairProgramming
+//
+//  Created by Emmet Malone on 11/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct JointPairProgrammingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
